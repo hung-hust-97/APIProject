9bd98ed627659352a170b7d7e58c91fb59845e4f
hung_hust <hunga1k15tv@gmail.com>
2019-07-09 14:37:37 +0700

    add all file

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const logger = require('morgan');
+const app = express();
+//MiddleWares
+
+
+app.use(logger('dev'));
+//Router
+app.get('/', (req, res, next) => {
+    res.status(200).json({
+        message : "You requested index page."
+    });
+});
+
+//Catch 404 Erroer 
+app.use((req, res, next) => {
+    const err = new Error("Not Found");
+    err.status =404;
+    next(err);
+});
+
+//Error handle function
+
+app.use((err, req, res, next) => {
+    const error = app.get('env') === 'development' ? err : {};
+     const status = err.status || 500;
+    //Respond to client 
+    res.status(status).json({
+        error : {
+            message : error.message
+        }
+    });
+    //Respond to ourselves
+    console.error(err);
+})
+
+//Start the server
+const port = app.get('port') || 3000;
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
